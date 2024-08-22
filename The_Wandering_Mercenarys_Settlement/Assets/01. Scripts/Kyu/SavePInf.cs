72ea189ef2824d59dca94ede0bee8380876fb0c4
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2024-08-22 15:33:14 +0900

    What is this?

---
+++ The_Wandering_Mercenarys_Settlement/Assets/01. Scripts/Kyu/SavePInf.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePInf
+{
+    public int R_Point = 0;
+    public int H_UpLvl;
+    public int P_UpLvl;
+}
